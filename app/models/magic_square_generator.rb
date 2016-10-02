576cce7958d07c51f832bcbd7c817783ed821935
John Arthorne <john.arthorne@shopify.com>
2016-10-02 17:20:52 -0400

    Row generator

--- app/models/magic_square_generator.rb
+++ app/models/magic_square_generator.rb
@@ -12,11 +12,25 @@ class MagicSquareGenerator
     full_sum = square_sum
     row_sum = full_sum / @dim
 
-    valid_rows = generate_rows
+    valid_rows = generate_all_rows
+
+
+    []
   end
 
   def square_sum
+    return @square_sum if @square_sum
     cells = @dim ** 2
-  	cells * (cells+1) / 2
+    square_sum = cells * (cells+1) / 2
+  end
+
+  def row_sum
+    square_sum / @dim
+  end
+
+  def generate_all_rows
+    (1..(@dim**2)).to_a.permutation(@dim).select{|row|
+      row.reduce(0, :+) == row_sum
+    }
   end
 end
--- test/models/magic_square_generator_test.rb
+++ test/models/magic_square_generator_test.rb
@@ -7,21 +7,28 @@ class MagicSquareGeneratorTest < ActionDispatch::IntegrationTest
   end
 
   # test "generates_four_dim_squares" do
-  # 	result = @generator.generate_squares(4)
+  #   result = @generator.generate_squares(4)
   #   assert_equal 880, result.size
   # end
 
   test 'handles invalid dimensions' do
-  	assert_raises {
+    assert_raises {
       MagicSquareGenerator.new(2).generate_squares
-  	}
-  	assert_raises {
+    }
+    assert_raises {
       MagicSquareGenerator.new(1).generate_squares
-  	}
+    }
   end
 
   test "computes sum of all squares" do
     assert_equal 1+2+3+4+5+6+7+8+9, MagicSquareGenerator.new(3).square_sum
     assert_equal 8*17, @generator.square_sum
+  end
+
+  test "generates correct rows" do
+    rows = @generator.generate_all_rows
+    rows.each do |row|
+      assert_equal 34, row.inject(0, :+)
+    end
   end
 end
